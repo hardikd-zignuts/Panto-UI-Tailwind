a6b49d54f53a30769a84b2e6ec2e650fff644938
Hardik Desai <hardikd@zignuts.com>
2023-05-22 19:00:23 +0530

    feat: :art: Started

---
+++ src/components/common/AppBar/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { HiShoppingBag } from 'react-icons/hi'
+import { Navbar, Dropdown } from 'flowbite-react'
+
+const AppBar = () => {
+    return (
+        <>
+            <Navbar
+                fluid={true}
+                rounded={true}
+            >
+                <Navbar.Brand href="https://flowbite.com/">
+                    <img
+                        src="https://flowbite.com/docs/images/logo.svg"
+                        className="mr-3 h-6 sm:h-9"
+                        alt="Flowbite Logo"
+                    />
+                    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+                        Flowbite
+                    </span>
+                </Navbar.Brand>
+                <div className="flex md:order-2">
+                    <div className='relative'>
+                        <HiShoppingBag size={30} />
+                        <div className="absolute right-[4px] top-[2px]">0</div>
+                    </div>
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Navbar.Link
+                        href="/navbars"
+                        active={true}
+                    >
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        <Dropdown
+                            label="Dropdown button"
+                            dismissOnClick={false}
+                        >
+                            <Dropdown.Item>
+                                Dashboard
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Settings
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Earnings
+                            </Dropdown.Item>
+                            <Dropdown.Item>
+                                Sign out
+                            </Dropdown.Item>
+                        </Dropdown>
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Services
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link href="/navbars">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </Navbar>
+        </>
+    )
+}
+export default AppBar
